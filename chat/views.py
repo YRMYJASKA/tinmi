3acad255e89f632d5f7869800385ef3675280e68
Jyry Hjelt <hjelt.jyry@gmail.com>
2018-12-08 00:35:59 +0100

    Added chatroom ID handling in the URL

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,15 @@
+from django.http import Http404
 from django.shortcuts import render, HttpResponse
+
+from .models import *
 
 def index(request):
     return HttpResponse("Index page placeholder")
+
+def chatroom(request, slug):
+    try:
+        r = Chatroom.objects.get(pk=slug)
+    except Chatroom.DoesNotExist:
+        raise Http404("Chatroom does not exist!")
+    return HttpResponse("Chatroom page placeholder")
+
