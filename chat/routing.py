59cb16ac59e0e5096d0789cd655f10ad7ad448c9
Jyry Hjelt <hjelt.jyry@gmail.com>
2019-01-20 22:11:34 +0100

    added wss:// to routing

--- chat/routing.py
+++ chat/routing.py
@@ -4,4 +4,5 @@ from . import consumers
 
 websocket_urlpatterns = [
         url(r'^ws/chat/(?P<slug>[^/]+)/$', consumers.ChatConsumer),
+        url(r'^wss/chat/(?P<slug>[^/]+)/$', consumers.ChatConsumer),
     ]
