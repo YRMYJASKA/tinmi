a67da9ea7d7a1c4ec66f5fb9e4d2a82abd321f1e
Jyry Hjelt <hjelt.jyry@gmail.com>
2019-01-20 22:31:34 +0100

    trying to fix ASGI

--- tinmi/asgi.py
+++ tinmi/asgi.py
@@ -5,8 +5,11 @@ defined in the ASGI_APPLICATION setting.
 
 import os
 import django
+import channels.asgi
 from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tinmi.settings")
 django.setup()
 application = get_default_application()
+channel_layer = channels.asgi.get_channel_layer()
+
